09c8eac21b0e3fe7c88fbba3284c5e4c7872e66c
gabriel-estudo <milgabriel551@gmail.com>
2025-11-13 14:24:15 -0300

    commit inicial

---
+++ src/components/Test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Test extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            message: "Ola mundo"
+        }
+    }
+
+    render() {
+        return <h1>{this.state.message}</h1>;
+    }
+}
+
+export default Test;
